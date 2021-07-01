27a91e1f36771fcf141f43fcda84e4e2d8276daf
issouf-sacko <57715649+issouf-sacko@users.noreply.github.com>
2021-07-01 15:15:11 +0200

    add MediaObject

---
+++ src/Entity/MediaObject.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MediaObjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MediaObjectRepository::class)
+ */
+class MediaObject
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $filename;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=room::class)
+     */
+    private $room;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    public function setFilename(string $filename): self
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    public function getRoom(): ?room
+    {
+        return $this->room;
+    }
+
+    public function setRoom(?room $room): self
+    {
+        $this->room = $room;
+
+        return $this;
+    }
+}
